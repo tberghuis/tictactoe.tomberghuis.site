3e54e4045e3e0ed8880b357331096c649e9486d5
Tom Berghuis <thomas.berghuis@gmail.com>
2019-09-10 10:33:07 +1000

    added cors to server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-module.exports = require("http").createServer();
+// module.exports = require("http").createServer();
+
+module.exports = require("http").createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000 // 30 days
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+});
